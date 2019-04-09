e357787464f5f84a407961422204c42c2a1ea2db
jeckbjy-work <jinyuan.bian@zenjoy.net>
2019-04-09 11:34:47 +0800

    etcd discovery

---
+++ etcd/discovery.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"test-etcd/discovery"
+)
+
+const (
+	kProvider = 1
+	kConsumer = 2
+)
+
+func main() {
+	role := flag.Int("role", 0, "provider|consumer")
+	flag.Parse()
+	log.Printf("role=%+v\n", *role)
+
+	endpoints := []string{":2379"}
+
+	if *role == kProvider {
+		discovery.StartProvider("test", "http://localhost:8080", endpoints)
+	} else if *role == kConsumer {
+		discovery.StartConsumer(endpoints)
+	} else {
+		log.Println("usage: discovery --role=[1|2]")
+	}
+}
--- inject/build.sh
+++ inject/build.sh
@@ -1,2 +1,5 @@
+#!/bin/bash
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}")" && pwd )
+
 BUILD_TIME=`date +"%s"`
 go build -ldflags "-X main.BuildTime=$BUILD_TIME" -o ../bin/inject
