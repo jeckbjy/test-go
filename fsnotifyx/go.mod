899967b6521678204767922ecd491947244bad53
jeckbjy-work <jinyuan.bian@zenjoy.net>
2019-12-19 13:54:40 +0800

    add fsnotify

---
+++ fsnotifyx/go.mod
@@ -0,0 +1,8 @@
+module fsnotifyx
+
+go 1.13
+
+require (
+	github.com/fsnotify/fsnotify v1.4.7
+	golang.org/x/sys v0.0.0-20191218084908-4a24b4065292 // indirect
+)
---
+++ fsnotifyx/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"io/ioutil"
+	"log"
+
+	"github.com/fsnotify/fsnotify"
+)
+
+func main() {
+	log.Printf("run fsnotify")
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	watcher.Add("registry.srv")
+	for {
+		select {
+		case event := <-watcher.Events:
+			switch {
+			case event.Op&fsnotify.Write == fsnotify.Write:
+				data, err := ioutil.ReadFile(event.Name)
+				log.Printf("Write: %s:%s,%s, %+v", event.Op, event.Name, data, err)
+			case event.Op&fsnotify.Create == fsnotify.Create:
+				log.Printf("Create: %s: %s", event.Op, event.Name)
+			case event.Op&fsnotify.Remove == fsnotify.Remove:
+				log.Printf("Remove: %s: %s", event.Op, event.Name)
+			case event.Op&fsnotify.Rename == fsnotify.Rename:
+				log.Printf("Rename: %s: %s", event.Op, event.Name)
+			case event.Op&fsnotify.Chmod == fsnotify.Chmod:
+				log.Printf("Chmod:  %s: %s", event.Op, event.Name)
+			}
+		case err := <-watcher.Errors:
+			log.Printf("%+v", err)
+		}
+	}
+}
