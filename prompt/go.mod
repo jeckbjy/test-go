55f18189357b8281d1d0354a98ea187c0c6d9001
bianjinyuan <bianjinyuan@bytedance.com>
2020-07-28 15:35:20 +0800

    prompt
    Change-Id: Iee2b4cc7c828e559961df54c46ffd156b02ba08a

---
+++ prompt/go.mod
@@ -0,0 +1,12 @@
+module prompt
+
+go 1.14
+
+require (
+	github.com/c-bata/go-prompt v0.2.3 // indirect
+	github.com/manifoldco/promptui v0.7.0 // indirect
+	github.com/mattn/go-colorable v0.1.7 // indirect
+	github.com/mattn/go-runewidth v0.0.9 // indirect
+	github.com/mattn/go-tty v0.0.3 // indirect
+	github.com/pkg/term v0.0.0-20200520122047-c3ffed290a03 // indirect
+)
---
+++ prompt/main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"strconv"
+
+	"github.com/c-bata/go-prompt"
+	"github.com/manifoldco/promptui"
+)
+
+func main() {
+	// example1()
+	example3()
+}
+
+func completer(d prompt.Document) []prompt.Suggest {
+	s := []prompt.Suggest{
+		{Text: "users", Description: "Store the username and age"},
+		{Text: "articles", Description: "Store the article text posted by user"},
+		{Text: "comments", Description: "Store the text commented to articles"},
+	}
+	return prompt.FilterHasPrefix(s, d.GetWordBeforeCursor(), true)
+}
+
+func example1() {
+	fmt.Println("Please select table.")
+	t := prompt.Input("> ", completer)
+	fmt.Println("You selected " + t)
+}
+
+func example2() {
+	validate := func(input string) error {
+		_, err := strconv.ParseFloat(input, 64)
+		if err != nil {
+			return errors.New("Invalid number")
+		}
+		return nil
+	}
+
+	prompt := promptui.Prompt{
+		Label:    "Number",
+		Validate: validate,
+	}
+
+	result, err := prompt.Run()
+
+	if err != nil {
+		fmt.Printf("Prompt failed %v\n", err)
+		return
+	}
+
+	fmt.Printf("You choose %q\n", result)
+}
+
+func example3() {
+	prompt := promptui.Select{
+		Label: "Select Day",
+		Items: []string{"Monday", "Tuesday", "Wednesday", "Thursday", "Friday",
+			"Saturday", "Sunday"},
+	}
+
+	_, result, err := prompt.Run()
+
+	if err != nil {
+		fmt.Printf("Prompt failed %v\n", err)
+		return
+	}
+
+	fmt.Printf("You choose %q\n", result)
+}
